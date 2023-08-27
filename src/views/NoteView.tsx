ecf343e297d310f8a8d580c80aec0a916f9c3da9
Michael Papesca <michael.papesca@onmail.com>
2023-08-27 02:44:07 -0600

    Allow scrolling without selecting notes input

--- src/views/NoteView.tsx
+++ src/views/NoteView.tsx
@@ -1,23 +1,45 @@
 import { Button, Divider, Input, Layout, Text, TopNavigation } from '@ui-kitten/components';
+import { Button as DefaultButton, Keyboard, PanResponder } from 'react-native';
 import { ViewWithInsets } from './ViewWithInsets';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import type { TRootStackParamList } from '../navigators/types';
 import { NoteViewTopNavigation } from '../components/note';
-import { KeyboardAvoidingView, Platform, View } from 'react-native';
+import { InputAccessoryView, KeyboardAvoidingView, Platform, View } from 'react-native';
+import { useEffect, useState } from 'react';
 
 type TNoteViewProps = NativeStackScreenProps<TRootStackParamList, 'Note'>;
 
 const NoteView = ({ route, navigation }: TNoteViewProps) => {
+  const [panResponder, setPanResponder] = useState<ReturnType<PanResponder['create']>>();
+
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      setPanResponder(
+        PanResponder.create({
+          onStartShouldSetPanResponderCapture: () => true,
+        }),
+      );
+    }
+  }, []);
+
   console.log({ canGoBack: navigation.canGoBack() });
   return (
     <ViewWithInsets>
       <NoteViewTopNavigation route={route} navigation={navigation} />
       <KeyboardAvoidingView
-        style={{ flex: 1, borderWidth: 1, borderColor: 'red' }}
+        {...(Platform.OS === 'ios' ? panResponder?.panHandlers : {})}
+        style={{ flex: 1 }}
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       >
-        <Input multiline={true} textStyle={{ minHeight: '100%' }} />
+        <Input multiline={true} textStyle={{ minHeight: '100%' }} inputAccessoryViewID='Note' />
       </KeyboardAvoidingView>
+      <InputAccessoryView nativeID='Note'>
+        <View>
+          <Button onPress={Keyboard.dismiss} appearance='ghost'>
+            Done
+          </Button>
+        </View>
+      </InputAccessoryView>
     </ViewWithInsets>
   );
 };
